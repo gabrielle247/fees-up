e5479b9d20a804cb3bf23b556639c980eeb08f1f
MrGee <133508930+gabrielle247@users.noreply.github.com>
2025-11-25 04:16:29 +0200

    Add keystore properties loading to build.gradle.kts
    Load keystore properties from key.properties file if it exists.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,12 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
